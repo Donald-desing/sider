cd76e0d5ccf79f85a507be99d0f36329d1729aec
Donald-Design <79831132+Donald-desing@users.noreply.github.com>
2025-01-26 15:47:08 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -12,11 +14,19 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error(err));
 
+// Routes
+const questionsRouter = require('./routes/questions');
+const answersRouter = require('./routes/answers');
+
+app.use('/questions', questionsRouter);
+app.use('/answers', answersRouter);
+
 // Basic route
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
