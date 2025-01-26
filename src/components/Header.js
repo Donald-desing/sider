b47fb4e0b6fe3bcc6d60359699c84e89547bf648
Donald-Design <79831132+Donald-desing@users.noreply.github.com>
2025-01-26 13:23:31 +0300

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: '#004080', color: 'white', padding: '10px 20px' }}>
+      <h1>Question and Answer Site</h1>
+    </header>
+  );
+};
+
+export default Header;
